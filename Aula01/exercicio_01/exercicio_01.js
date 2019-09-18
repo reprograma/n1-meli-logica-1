2eae13952c4e70be90d560ff534eb59ab3928c6e
myrmanso <mayaramanso.devel@gmail.com>
2019-09-18 13:04:14 -0300

    adiciona resolução do exercicio em js

---
+++ Aula01/exercicio_01/exercicio_01.js
@@ -0,0 +1,11 @@
+/*
+Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10.
+*/
+
+const num = parseInt(prompt('Digite um número'));
+
+if(num > 10){
+    alert(`${num} é maior que 10`)
+}else{
+    alert(`${num} é menao que 10`)
+}
